2e249eccc10cafd7a0cce8da8e983f5f75e46756
Quinn Dizon <qdizon@gmail.com>
2019-09-18 06:16:19 -0400

    updated content

--- src/components/contentComponents/Unemployed.js
+++ src/components/contentComponents/Unemployed.js
@@ -5,7 +5,7 @@ const Unemployment = () => {
         <div>
             <h2>What is unemployment like in these communities?</h2>
             <p>
-                On average, more than 100 people claim unemployment per zip code in areas served by <strong>Courtyard</strong> and <strong>Park View</strong>. Both <strong>Hillcrest</strong> and <strong>Park View</strong> serve communities with high unemployment in Alaska. The more rural areas served by <strong>Riverview</strong> report some of the lowest unemployment numbers in the dataset. 
+                On average, more than 100 people claim unemployment per zip code in areas served by <strong>Courtyard</strong> and <strong>Park View</strong>. <strong>Hillcrest</strong>, <strong>Park Place</strong>, and <strong>Park View</strong> serve communities with high unemployment in Alaska. The more rural areas served by <strong>Riverview</strong> report some of the lowest unemployment numbers in the dataset. 
             </p>
         </div>
     )
